814342833bcedc3a0568b630e5eb8d0211f1854f
Bozhidar Mindov <bbm210@aubg.edu>
2023-07-18 00:19:33 +0300

    Add the login and register components to the app routing

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomePageComponent } from './home-page/home-page.component';
 import { ColorResultsComponent } from './color-results/color-results.component';
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'color-results', component: ColorResultsComponent }
+  { path: 'color-results', component: ColorResultsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
