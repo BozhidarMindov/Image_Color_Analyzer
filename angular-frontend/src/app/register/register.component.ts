4d5bb27c3fa35d2dd3022d8de78671a59937eefe
Bozhidar Mindov <bbm210@aubg.edu>
2023-07-19 21:41:04 +0300

    Ensure that the password is at least 8 characters long

--- angular-frontend/src/app/register/register.component.ts
+++ angular-frontend/src/app/register/register.component.ts
@@ -11,24 +11,36 @@ export class RegisterComponent {
   username: string = '';
   email: string = '';
   password: string = '';
+  formSubmitted: boolean = false;
   errorMessage: string = '';
 
   constructor(private router: Router, private authService: AuthService) { }
 
   register(): void {
+    this.formSubmitted = true;
+
+    // Check if all fields are filled
     if (!this.username || !this.email || !this.password) {
       this.errorMessage = 'Please fill in all fields.';
       return;
     }
 
+    // Check if the password is at least 8 characters long
+    if (this.password.length < 8){
+      this.errorMessage = 'Ensure that the password is at least 8 characters long.'
+      return
+    }
+
     this.authService.register(this.username, this.email, this.password).subscribe(
       () => {
         // Handle successful registration
+        alert('Account created successfully. Please login.');
         this.router.navigate(['/login']);
       },
       (error) => {
         if (error.status === 409) {
-          this.errorMessage = error.error.message; // Set the error message from the backend
+          alert('Username is already taken. Please login instead.');
+          this.router.navigate(['/login']);
         } else {
           // Handle registration error
         }
@@ -36,4 +48,5 @@ export class RegisterComponent {
     );
   }
 }
+
 
