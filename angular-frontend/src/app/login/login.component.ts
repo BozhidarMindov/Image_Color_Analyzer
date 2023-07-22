507879968360ed12e2221bce9393b7adfd4a54bb
Bozhidar Mindov <bbm210@aubg.edu>
2023-07-22 10:00:25 +0300

    Add necessary functionality to make login component work

--- angular-frontend/src/app/login/login.component.ts
+++ angular-frontend/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { AuthService} from "../auth.service";
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,20 +8,53 @@ import { AuthService} from "../auth.service";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  username: string = '';
+  email: string = '';
   password: string = '';
+  errorMessage: string = '';
 
-  constructor(private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   login(): void {
-    this.authService.login(this.username, this.password).subscribe(
-      (response) => {
+    // Check if all fields are filled
+    if (!this.email || !this.password) {
+      this.errorMessage = 'Please fill in all fields.';
+      return;
+    }
+
+    // Check if the email is valid
+    if (!this.validateEmail(this.email)) {
+      this.errorMessage = 'Please enter a valid email address.';
+      return;
+    }
+
+    // Check if the password is at least 8 characters long
+    if (this.password.length < 8) {
+      this.errorMessage = 'Password must be at least 8 characters long.';
+      return;
+    }
+
+    // Attempt user login
+    this.authService.login(this.email, this.password).subscribe(
+      () => {
         // Handle successful login
+        this.router.navigate(['/']);
       },
       (error) => {
-        // Handle login error
+        if (error.status === 409) {
+          this.errorMessage = 'Invalid email or password. Please try again.';
+        } else {
+          this.errorMessage = 'An error occurred during login. Please try again later.';
+        }
       }
     );
   }
+
+  // Function to validate email format
+  private validateEmail(email: string): boolean {
+    // Regular expression to validate email format
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+  }
 }
+
 
