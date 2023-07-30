6bb546977fa43c7a7ab774404934ae690e90e11d
Bozhidar Mindov <bbm210@aubg.edu>
2023-07-30 21:58:23 +0300

    Render the navabar and footer components only once upon loading the app

--- angular-frontend/src/app/navbar/navbar.component.ts
+++ angular-frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {AuthService} from "../auth.service";
 import {Router} from "@angular/router";
 
@@ -7,13 +7,13 @@ import {Router} from "@angular/router";
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit{
   isCollapsed = true;
   currentUser =  "";
 
   constructor(private router: Router, private authService: AuthService) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.getCurrentUser();
   }
   getCurrentUser(): void {
