e826a5573eee758aac574ba31f2d8b69720bfe0c
Bozhidar Mindov <bbm210@aubg.edu>
2023-07-30 22:08:27 +0300

    Use ngOnInit rather than ngAfterViewInit

--- angular-frontend/src/app/navbar/navbar.component.ts
+++ angular-frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../auth.service";
 import {Router} from "@angular/router";
 
@@ -7,13 +7,13 @@ import {Router} from "@angular/router";
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements AfterViewInit{
+export class NavbarComponent implements OnInit{
   isCollapsed = true;
   currentUser =  "";
 
   constructor(private router: Router, private authService: AuthService) { }
 
-  ngAfterViewInit(): void {
+  ngOnInit(): void {
     this.getCurrentUser();
   }
   getCurrentUser(): void {
