02254bd070bda687e62ec1c03c67fc5b9241da8d
Bozhidar Mindov <bbm210@aubg.edu>
2023-08-06 22:53:03 +0300

    Re-style login and register components. Don't show navbar and footer while using them

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,9 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent{
+   constructor(private router: Router) { }
+
+  shouldShowNavbar(): boolean {
+     // Check if the current route is the login or register page
+     const currentRoute = this.router.url;
+     return !(currentRoute.includes('/login') || currentRoute.includes('/register'));
+  }
+
+  shouldShowFooter(): boolean {
+      // Check if the current route is the login or register page
+     const currentRoute = this.router.url;
+     return !(currentRoute.includes('/login') || currentRoute.includes('/register'));
+  }
 }
